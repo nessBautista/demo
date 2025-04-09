d55920d6153854839a9724c85aa17d18703965f5
Ness <nestor.jbautista@gmail.com>
2025-04-09 11:12:01 -0600

    Auto-commit: Test for Github PR tool

---
+++ create_a_python_script_that_prints_hello.py
@@ -0,0 +1,8 @@
+def print_hello_world():
+    """
+    Prints 'Hello, World!' to the console.
+    """
+    print("Hello, World!")
+
+# Call the function to print 'Hello, World!'
+print_hello_world()
